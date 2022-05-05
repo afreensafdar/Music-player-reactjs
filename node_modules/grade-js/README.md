4d147e9115122e90ee435d30de6fcd921d6f84ff
afreen <afreen.leo502@gmail.com>
2022-05-05 12:48:57 -0500

    music app and readme update

---
+++ node_modules/grade-js/README.md
@@ -0,0 +1,110 @@
+# Grade
+
+## Demo
+
+[Check it out](http://benhowdle.im/grade)
+
+## Install
+
+Download this repo and grab the `grade.js` file from the `/docs/dist` folder.
+
+Or install from npm: `npm install grade-js`
+
+Use the CDN link:
+
+`https://unpkg.com/grade-js/docs/dist/grade.js`
+
+## Usage
+
+Recommended HTML structure:
+
+```html
+<!--the gradients will be applied to these outer divs, as background-images-->
+<div class="gradient-wrap">
+    <img src="./samples/finding-dory.jpg" alt="" />
+</div>
+<div class="gradient-wrap">
+    <img src="./samples/good-dinosaur.jpg" alt="" />
+</div>
+```
+
+If you have the `grade.js` in your project, you can include it with a script tag and initialise it like so:
+
+```html
+<script src="path/to/grade.js"></script>
+<script type="text/javascript">
+    window.addEventListener('load', function(){
+        /*
+            A NodeList of all your image containers (Or a single Node).
+            The library will locate an <img /> within each
+            container to create the gradient from.
+         */
+        Grade(document.querySelectorAll('.gradient-wrap'))
+    })
+</script>
+```
+
+If you pass in a 3rd parameter and it's a function, the HTML element(s) you passed in as the 1st parameter will **not** be manipulated, but an array will be returned to you, for you to do as you please with, ie.
+```javascript
+Grade(document.querySelectorAll('.gradient-wrap'), null, function(gradientData){
+    // sample contents of `gradientData` can be inspected here https://jsonblob.com/57c4601ee4b0dc55a4f180f1
+})
+```
+
+If you've installed from npm, you can use the library like so:
+
+```javascript
+import Grade from 'grade-js'
+// initialise as above
+```
+
+The module this imports will be using ES2015 syntax, so it will need to be transpiled by a build tool, like [Babel](https://babeljs.io/), and if you are importing the module in this fashion (and using npm), I imagine you're already using a bundling tool, like Webpack or Browserify!
+
+## Running locally
+
+If you want to run this locally, just to test it, you need to serve `index.html` via a webserver, not just by opening it in a browser, else the browser will throw a security error. I would recommend either [live-server](https://www.npmjs.com/package/live-server) (requires Node.js installed on your machine) or if you have Python installed, just run `python -m SimpleHTTPServer` inside the project root. If you're on Windows, I believe WAMP/Apache is the best way to go.
+
+## Remote images
+
+This plugin utilises the `<canvas>` element and the `ImageData` object, and due to cross-site security limitations, the script will fail if one tries to extract the colors from an image not hosted on the current domain, *unless* the image allows for [Cross Origin Resource Sharing](http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+
+__Enabling CORS on S3__
+
+To enable CORS for images hosted on S3 buckets, follow the Amazon guide [here](http://docs.aws.amazon.com/AmazonS3/latest/UG/EditingBucketPermissions.html); adding the following to the bucket's CORS configuration:
+
+```xml
+<CORSRule>
+ <AllowedOrigin>*</AllowedOrigin>
+ <AllowedMethod>GET</AllowedMethod>
+</CORSRule>
+```
+
+For all images, you can optionally also include a cross-origin attribute in your image.
+
+```html
+<img src="/image.jpg" cross-origin="anonymous"/>
+```
+
+## License
+
+MIT License
+
+Copyright (c) 2016 Ben Howdle
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ node_modules/grade-js/src/index.js
@@ -0,0 +1,179 @@
+const prefixes = ['webkit'];
+
+class Grade {
+    constructor(container, img_selector, callback) {
+        this.callback = callback || null
+        this.container = container;
+        this.image = this.container.querySelector(img_selector) || this.container.querySelector('img')
+        this.gradientData = []
+        if(!this.image || !this.container){
+            return
+        }
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.imageDimensions = {
+            width: 0,
+            height: 0
+        };
+        this.imageData = [];
+        this.readImage()
+    }
+
+    readImage() {
+        this.imageDimensions.width = this.image.width * 0.1;
+        this.imageDimensions.height = this.image.height * 0.1;
+        this.render()
+    }
+
+    getImageData() {
+        let imageData = this.ctx.getImageData(
+            0, 0, this.imageDimensions.width, this.imageDimensions.height
+        ).data;
+        this.imageData = Array.from(imageData)
+    }
+
+    getChunkedImageData() {
+        const perChunk = 4;
+
+        let chunked = this.imageData.reduce((ar, it, i) => {
+            const ix = Math.floor(i / perChunk)
+            if (!ar[ix]) {
+                ar[ix] = []
+            }
+            ar[ix].push(it);
+            return ar
+        }, []);
+
+        let filtered = chunked.filter(rgba => {
+            return rgba.slice(0, 2).every(val => val < 250) && rgba.slice(0, 2).every(val => val > 0)
+        });
+
+        return filtered
+    }
+
+    getRGBAGradientValues(top) {
+        return top.map((color, index) => {
+            return `rgb(${color.rgba.slice(0, 3).join(',')}) ${index == 0 ? '0%' : '75%'}`
+        }).join(',')
+    }
+
+    getCSSGradientProperty(top) {
+        const val = this.getRGBAGradientValues(top);
+        return prefixes.map(prefix => {
+            return `background-image: -${prefix}-linear-gradient(
+                        135deg,
+                        ${val}
+                    )`
+        }).concat([`background-image: linear-gradient(
+                    135deg,
+                    ${val}
+                )`]).join(';')
+    }
+
+    getMiddleRGB(start, end) {
+        let w = 0.5 * 2 - 1;
+        let w1 = (w + 1) / 2.0;
+        let w2 = 1 - w1;
+        let rgb = [parseInt(start[0] * w1 + end[0] * w2), parseInt(start[1] * w1 + end[1] * w2), parseInt(start[2] * w1 + end[2] * w2)];
+        return rgb;
+    }
+
+    getSortedValues(uniq) {
+        const occurs = Object.keys(uniq).map(key => {
+                const rgbaKey = key;
+                let components = key.split('|'),
+                    brightness = ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
+                return {
+                    rgba: rgbaKey.split('|'),
+                    occurs: uniq[key],
+                    brightness
+                }
+            }).sort((a, b) => a.occurs - b.occurs).reverse().slice(0, 10);
+        return occurs.sort((a, b) => a.brightness - b.brightness).reverse()
+    }
+
+    getTextProperty(top) {
+        let rgb = this.getMiddleRGB(top[0].rgba.slice(0,3), top[1].rgba.slice(0,3));
+        let o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) /1000);
+        if (o > 125) {
+            return 'color: #000';
+        } else { 
+            return 'color: #fff';
+        }
+    }
+
+    getTopValues(uniq) {
+        let sorted = this.getSortedValues(uniq);
+        return [sorted[0], sorted[sorted.length - 1]]
+    }
+
+    getUniqValues(chunked) {
+        return chunked.reduce((accum, current) => {
+            let key = current.join('|');
+            if (!accum[key]) {
+                accum[key] = 1;
+                return accum
+            }
+            accum[key] = ++(accum[key]);
+            return accum
+        }, {})
+    }
+
+    renderGradient() {
+        const ls = window.localStorage;
+        const item_name = `grade-${this.image.getAttribute('src')}`;
+        let top = null;
+
+        if (ls && ls.getItem(item_name)) {
+            top = JSON.parse(ls.getItem(item_name));
+        } else {
+            let chunked = this.getChunkedImageData();
+            top = this.getTopValues(this.getUniqValues(chunked));
+
+            if (ls) {
+                ls.setItem(item_name, JSON.stringify(top));
+            }
+        }
+
+        if(this.callback){
+            this.gradientData = top
+            return
+        }
+
+        let gradientProperty = this.getCSSGradientProperty(top);
+
+        let textProperty = this.getTextProperty(top);
+
+        let style = `${this.container.getAttribute('style') || ''}; ${gradientProperty}; ${textProperty}`;
+        this.container.setAttribute('style', style)
+    }
+
+    render() {
+        this.canvas.width = this.imageDimensions.width;
+        this.canvas.height = this.imageDimensions.height;
+        this.ctx.drawImage(this.image, 0, 0, this.imageDimensions.width, this.imageDimensions.height);
+        this.getImageData();
+        this.renderGradient();
+    }
+}
+
+module.exports = (containers, img_selector, callback) => {
+    const init = (container, img_selector, callback) => {
+        let grade = new Grade(container, img_selector, callback),
+            gradientData = grade.gradientData
+        if(!gradientData.length){
+            return null
+        }
+        return {
+            element: container,
+            gradientData
+        }
+    }
+    let results = (NodeList.prototype.isPrototypeOf(containers)
+    ? Array.from(containers).map(container => init(container, img_selector, callback))
+    : [init(containers, img_selector, callback)]).filter(Boolean)
+
+    if(results.length){
+        return callback(results)
+    }
+};
